5813221da0a4b1ce00676c8d559724eef931f3a2
Nick <Bauer_Nicholas@bah.com>
2019-11-13 16:15:52 -0500

    Updated withRouter Navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import './App.css';
-//import HomePage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import { Route, Link } from 'react-router-dom';
 
+const HatsPage = props => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/hats' component={HatsPage} />
+    </div>
+  );
+}
+
+/* OLD STUFF DONT THINK I NEED
 const HomePage = props => (
   <div>
     <button onClick={() => props.history.push('/topics')}>Topics</button>
@@ -10,9 +26,12 @@ const HomePage = props => (
   </div>
 );
 
-const TopicsList = () => (
+const TopicsList = props => (
   <div>
     <h1>TOPIC LIST PAGE</h1>
+    <Link to={`${props.match.url}/1`}>TO TOPIC 1</Link><br />
+    <Link to={`${props.match.url}/2`}>TO TOPIC 2</Link><br />
+    <Link to={`${props.match.url}/3`}>TO TOPIC 3</Link><br />
   </div>
 );
 
@@ -20,16 +39,6 @@ const TopicDetail = props => (
   <div>
     <h1>TOPIC DETAIL PAGE {props.match.params.topicId}</h1>
   </div>
-);
-
-function App() {
-  return (
-    <div>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/topics' component={TopicsList}/>
-      <Route path='/topics/:topicId' component={TopicDetail}/>
-    </div>
-  );
-}
+);*/
 
 export default App;
