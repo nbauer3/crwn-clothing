f6bbbfa6f14939d0e006700ab829917f2893dafa
Nick <Bauer_Nicholas@bah.com>
2019-11-15 00:29:01 -0500

    Sign-in Form component & styling added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component';
 import { Route } from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component.jsx';
-import Header from './components/header/header.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Header/>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUp} />
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -16,6 +16,9 @@ const Header = () => (
             <Link className='option'>
                 CONTACT
             </Link>
+            <Link className='option' to='/signin'>
+                SIGN-IN
+            </Link>
         </div>
     </div>
 )
