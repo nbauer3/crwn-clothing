ec1944d179ce0a93927f368687a9a1d384d766ce
Nick <Bauer_Nicholas@bah.com>
2019-11-18 16:06:17 -0500

    Added Further Authentication Functionality

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -20,10 +20,11 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
+      //this.setState({ currentUser: user });
+      createUserProfileDocument(user);
 
-      console.log(user);
+      //console.log(user);
     });
   }
 
